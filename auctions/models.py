f7ccaf5c62ac1f31ca030e6c6b11cbb4961a6748
Alejandro_Ferrera <aferrerah1421@gmail.com>
2022-09-20 04:19:08 -0300

    added Category and Listing models

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,35 @@
+from pyexpat import model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
+    pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.id}: {self.name}"
+
+class Listing(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300, blank=True)
+    starting_bid = models.DecimalField(max_digits=9, decimal_places=2)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    image_url = models.URLField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+    winner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="wins")
+
+    def __str__(self):
+        return f"{self.id}: {self.title} ${self.price}"
+
+
+class Bid(models.Model):
+    pass
+
+class Comment(models.Model):
     pass
