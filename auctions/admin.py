e2b46b54028ceec32131a73d7232d5d2ba76e05f
Alejandro_Ferrera <aferrerah1421@gmail.com>
2022-09-20 15:25:18 -0300

    added Bid and Comment models and customize admin panel

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Category, Listing
+from .models import Bid, Comment, User, Category, Listing
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "owner", "title", "starting_bid", "price", "active", "winner")
+    filter_horizontal = ("watched",)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id","user", "listing", "created", "text")
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "listing", "last_bided", "amount")
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Category)
-admin.site.register(Listing)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Bid, BidAdmin)
--- auctions/models.py
+++ auctions/models.py
@@ -1,10 +1,11 @@
-from pyexpat import model
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
+    # The user's watchlist is created in listing model with a many to many relation between listing and user.
+    # This user model has default fields from Django Abstract User (username, password, etc). 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -15,21 +16,35 @@ class Category(models.Model):
 class Listing(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=300, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=700, blank=True)
     starting_bid = models.DecimalField(max_digits=9, decimal_places=2)
     price = models.DecimalField(max_digits=9, decimal_places=2)
     image_url = models.URLField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
     winner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="wins")
+    watched = models.ManyToManyField(User, related_name="watchlist", blank=True) #Watchlist
 
     def __str__(self):
-        return f"{self.id}: {self.title} ${self.price}"
+        return f"{self.title} ${self.price}"
 
 
 class Bid(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
+    last_bided = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.user} on {self.listing} at {self.last_bided} bids: ${self.amount}"
 
 class Comment(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    text = models.CharField(max_length=700)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} on {self.listing} at {self.created}: \n {self.text}"
+
